a0ca4708026e2b8420ae64c621c2cc07673cc376
byarti <chinarireddy2005@gmail.com>
2025-09-21 09:15:49 +0530

    Modified

--- Linked List/Singly Linked List/search_element.c
+++ Linked List/Singly Linked List/search_element.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node{
+struct Node {
     int data;
     struct Node* next;
 };
 
-int search(struct Node** head, int key){
+void insertEnd(struct Node** head, int data){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (*head == NULL){
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void Print(struct Node* head){
+    struct Node* temp = head;
+    while (temp != NULL){
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int Search(struct Node* head, int key){
     struct Node* temp = head;
     int pos = 1; 
-    while(temp != NULL){
+    while (temp != NULL){
         if (temp->data == key) return pos;
         temp = temp->next;
         pos++;
@@ -17,17 +42,38 @@ int search(struct Node** head, int key){
     return -1;
 }
 
-int count(struct Node** head){
-    int count = 0;
+int count(struct Node* head){
+    int cnt = 0;
     struct Node* temp = head;
-    while(temp != NULL){
-        count++;
+    while (temp != NULL){
+        cnt++;
         temp = temp->next;
     }
-    return count;
+    return cnt;
 }
 
 int main(){
-    
-}
+    struct Node* head = NULL;
+    int search;
 
+    insertEnd(&head, 10); 
+    insertEnd(&head, 20); 
+    insertEnd(&head, 30); 
+    insertEnd(&head, 40); 
+    insertEnd(&head, 50);
+
+    Print(head);
+
+    printf("Value you wanna search from the LL: ");
+    scanf("%d", &search);
+
+    int pos = Search(head, search);
+    if (pos != -1)
+        printf("Value %d found at position %d\n", search, pos);
+    else
+        printf("Value %d not found in the list\n", search);
+
+    printf("Total nodes in the list: %d\n", count(head));
+
+    return 0;
+}
